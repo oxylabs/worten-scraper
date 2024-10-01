3edc7ca3ae03d8058e025e21797b38b9a5575a99
OxylabsOrg <103110131+OxylabsOrg@users.noreply.github.com>
2024-10-01 14:52:02 +0300

    Update worten_csharp.cs

--- code examples/worten_csharp.cs
+++ code examples/worten_csharp.cs
@@ -15,7 +15,7 @@ namespace OxyApi
 
             var parameters = new Dictionary<string, string>()
             {
-                { "source", "universal_ecommerce" },
+                { "source", "universal" },
                 { "url", "https://www.worten.pt/promocoes/pequenos-eletrodomesticos" },
             };
 
