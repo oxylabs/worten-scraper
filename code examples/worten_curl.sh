f04d1115d8be59f17624b832486a968c1345dc91
OxylabsOrg <103110131+OxylabsOrg@users.noreply.github.com>
2024-10-01 14:52:19 +0300

    Update worten_curl.sh

--- code examples/worten_curl.sh
+++ code examples/worten_curl.sh
@@ -1,4 +1,4 @@
 curl --user user:pass \
 'https://realtime.oxylabs.io/v1/queries' \
 -H "Content-Type: application/json" \
--d '{"source": "universal_ecommerce", "url": "https://www.worten.pt/promocoes/pequenos-eletrodomesticos"}'
+-d '{"source": "universal", "url": "https://www.worten.pt/promocoes/pequenos-eletrodomesticos"}'
