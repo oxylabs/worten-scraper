89f4a37611fbd677f10c67440f2ac354df9e315d
OxylabsOrg <103110131+OxylabsOrg@users.noreply.github.com>
2024-10-01 14:52:47 +0300

    Update worten_java.java

--- code examples/worten_java.java
+++ code examples/worten_java.java
@@ -8,7 +8,7 @@ public class Main implements Runnable {
 
     public void run() {
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("source", "universal_ecommerce");
+        jsonObject.put("source", "universal");
         jsonObject.put("url", "https://www.worten.pt/promocoes/pequenos-eletrodomesticos");
 
         Authenticator authenticator = (route, response) -> {
